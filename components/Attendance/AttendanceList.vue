c1181aa2d487ce75e9ce06a55013b8ad7acc0d2f
Rihan <me@file.properties>
2023-09-30 16:46:43 +0000

    refactor: :recycle: separate recent attendance

---
+++ components/Attendance/AttendanceList.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Attendance } from "~/types";
+
+const attendance = ref<Attendance[]>([]);
+</script>
+
+<template>
+  <aside
+    id="sidebar"
+    class="order-2 sm:order-1 md:w-96 sm:w-1/2 border border-zinc-200 dark:border-zinc-700 rounded-lg m-1.5 [@media(display-mode:standalone)]:mt-0 p-2 sm:p-3 md:px-3 transition-all overflow-y-scroll flex flex-col gap-2 [&::-webkit-scrollbar]:w-[7px] [&::-webkit-scrollbar-thumb]:bg-zinc-200 [&::-webkit-scrollbar-thumb]:rounded-lg [&::-webkit-scrollbar-thumb]:hover:bg-zinc-300/90 dark:[&::-webkit-scrollbar-thumb]:bg-zinc-800 dark:[&::-webkit-scrollbar-thumb]:rounded-lg dark:[&::-webkit-scrollbar-thumb]:hover:bg-zinc-700"
+  >
+    <h2 class="font-semibold text-lg mb-1">Recent Attendance</h2>
+
+    <div
+      v-if="attendance.length"
+      class="flex flex-col gap-4 divide-y divide-zinc-200 dark:divide-zinc-700"
+    >
+      <AttendanceCard
+        v-for="item in attendance"
+        :key="item.createdAt"
+        :attendance="item"
+        class="pt-3 first:pt-0 last:pb-0"
+      />
+    </div>
+
+    <div
+      v-else
+      class="text-zinc-700 flex flex-col items-center justify-center grow"
+    >
+      <UIcon name="i-heroicons-calendar-days" class="text-5xl" />
+      <p class="font-semibold text-lg mt-2 mb-3">No recent attendance</p>
+    </div>
+  </aside>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { Attendance } from "~/types";
-
 useHead({ title: "Attendance" });
 
 const { $pwa } = useNuxtApp();
@@ -13,40 +11,13 @@ async function submit() {
 
 const setFocus = refAutoReset(true, 1);
 onKeyStroke((e) => (setFocus.value = false));
-
-const attendance = ref<Attendance[]>([]);
 </script>
 
 <template>
   <div
     class="flex sm:h-screen flex-col sm:flex-row text-zinc-800 dark:text-white"
   >
-    <aside
-      id="sidebar"
-      class="order-2 sm:order-1 md:w-96 sm:w-1/2 border border-zinc-200 dark:border-zinc-700 rounded-lg m-1.5 [@media(display-mode:standalone)]:mt-0 p-2 sm:p-3 md:px-3 transition-all overflow-y-scroll flex flex-col gap-2"
-    >
-      <h2 class="font-bold text-xl mb-2">Recent Attendance</h2>
-
-      <div
-        v-if="attendance.length"
-        class="flex flex-col gap-2 divide-y divide-zinc-200 dark:divide-zinc-700"
-      >
-        <AttendanceCard
-          v-for="item in attendance"
-          :key="item.createdAt"
-          :attendance="item"
-          class="py-1 first:pt-1 last:pb-0"
-        />
-      </div>
-
-      <div
-        v-else
-        class="text-zinc-700 flex flex-col items-center justify-center grow"
-      >
-        <UIcon name="i-heroicons-calendar-days" class="text-5xl" />
-        <p class="font-semibold text-lg mt-2 mb-3">No recent attendance</p>
-      </div>
-    </aside>
+    <AttendanceList />
 
     <main
       class="order-1 sm:order-2 flex grow items-center justify-center relative h-screen"
@@ -65,21 +36,3 @@ const attendance = ref<Attendance[]>([]);
     </main>
   </div>
 </template>
-
-<style>
-/* width */
-#sidebar::-webkit-scrollbar {
-  width: 8px;
-}
-
-/* Handle */
-#sidebar::-webkit-scrollbar-thumb {
-  background: theme("colors.zinc.800");
-  border-radius: 10px;
-}
-
-/* Handle on hover */
-#sidebar::-webkit-scrollbar-thumb:hover {
-  background: theme("colors.zinc.700");
-}
-</style>
